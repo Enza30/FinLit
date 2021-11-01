cfa191a71fb0a0a8a2f3fcdcc3e892ee46e87f5f
Farendza <farendza.muda@gmail.com>
2021-11-01 09:46:43 +0700

    Initial Commit

---
+++ FinLit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FinLit
+//
+//  Created by Farendza Muda on 01/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FinLit/FinLitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinLitApp.swift
+//  FinLit
+//
+//  Created by Farendza Muda on 01/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FinLitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
