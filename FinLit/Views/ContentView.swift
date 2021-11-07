4f9d319707a96be90420534836758b8f4de84094
Farendza <farendza.muda@gmail.com>
2021-11-08 03:56:15 +0700

    Custom Tab Bar and Color pallette added

--- FinLit/Views/ContentView.swift
+++ FinLit/Views/ContentView.swift
@@ -9,36 +9,91 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var selectedIndex = 0
+    @State var shouldShowModel = false
+    
+    let tabBarImages = ["house.fill","banknote.fill","plus.app.fill","chart.bar.fill","book.closed.fill"]
+    
     var body: some View {
-            TabView {
-                Dashboard()
-                    .tabItem{
-                        Image(systemName: "house.fill")
-                        Text("Dashboard")
-                    }
+        VStack(spacing: 0){
+            ZStack{
                 
-                Budget()
-                    .tabItem{
-                        Image(systemName: "banknote.fill")
-                        Text("Budget")
-                    }
+                Spacer()
+                    .fullScreenCover(isPresented: $shouldShowModel, content: {
+                        Button(action: {shouldShowModel.toggle()}, label: {
+                            Text("Ini Input Expense Page")
+                        })
+                    })
                 
-                Evaluation()
-                    .tabItem{
-                        Image(systemName: "chart.bar.fill")
-                        Text("Evaluation")
-                    }
-                
-                Education()
-                    .tabItem{
-                        Image(systemName: "book.closed.fill")
-                        Text("Education")
-                    }
-        
+                switch selectedIndex{
+                case 1 :
+                    Budget()
+                case 3:
+                    Evaluation()
+                case 4:
+                    Education()
+                default:
+                    Dashboard()
+                }
+            }
+            Divider()
+                .padding(.bottom, 8)
+            HStack{
+                ForEach(0..<5) { num in
+                    Button(action:{
+                        if num == 2 {
+                            shouldShowModel.toggle()
+                            return
+                        }
+                        selectedIndex = num
+                    }, label: {
+                        Spacer()
+                        
+                        if num == 2 {
+                            Image(systemName: tabBarImages[num])
+                                .font(.system(size: 44, weight: .bold))
+                                .foregroundColor(.red)
+                        } else {
+                            Image(systemName: tabBarImages[num])
+                                .font(.system(size: 24, weight: .bold))
+                                .foregroundColor(selectedIndex == num ? Color(.black) : .init(white: 0.8))
+                        }
+                        
+                        Spacer()
+                    })
+                    
+                }
+            }
         }
     }
 }
 
+//TabView {
+//    Dashboard()
+//        .tabItem{
+//            Image(systemName: "house.fill")
+//            Text("Dashboard")
+//        }
+//
+//    Budget()
+//        .tabItem{
+//            Image(systemName: "banknote.fill")
+//            Text("Budget")
+//        }
+//
+//    Evaluation()
+//        .tabItem{
+//            Image(systemName: "chart.bar.fill")
+//            Text("Evaluation")
+//        }
+//
+//    Education()
+//        .tabItem{
+//            Image(systemName: "book.closed.fill")
+//            Text("Education")
+//        }
+//
+//}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
