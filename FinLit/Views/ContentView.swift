7dfe5fb7e7545fcdac8b24db582aaed318cf1854
Farendza <farendza.muda@gmail.com>
2021-11-09 09:44:08 +0700

    ganti tab bar yg lama

--- FinLit/Views/ContentView.swift
+++ FinLit/Views/ContentView.swift
@@ -9,105 +9,106 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var selectedIndex = 0
-    @State var shouldShowModel = false
-    
-    let tabBarImages = ["house.fill","banknote.fill","plus.app.fill","chart.bar.fill","book.closed.fill"]
-    let tabBarNames = ["Dashboard","Budget","   ","Evaluation","Education"]
+//    @State var selectedIndex = 0
+//    @State var shouldShowModel = false
+//    
+//    let tabBarImages = ["house.fill","banknote.fill","plus.app.fill","chart.bar.fill","book.closed.fill"]
+//    let tabBarNames = ["Dashboard","Budget","   ","Evaluation","Education"]
     
     var body: some View {
-        VStack(spacing: 0){
-            ZStack{
-                
-                Spacer()
-                    .fullScreenCover(isPresented: $shouldShowModel, content: {
-                        Button(action: {shouldShowModel.toggle()}, label: {
-                            Text("Ini Input Expense Page")
-                        })
-                    })
-                
-                switch selectedIndex{
-                case 1 :
-                    Budget()
-                case 3:
-                    Evaluation()
-                case 4:
-                    Education()
-                default:
-                    Dashboard()
+        TabView {
+            Dashboard()
+                .tabItem{
+                    Image(systemName: "house.fill")
+                    Text("Dashboard")
                 }
-            }
-            Divider()
-                .padding(.bottom, 8)
-            HStack{
-                ForEach(0..<5) { num in
-                        Button(action:{
-                            if num == 2 {
-                                shouldShowModel.toggle()
-                                return
-                            }
-                            selectedIndex = num
-                        }, label: {
-                            Spacer()
-                            
-                            if num == 2 {
-                                Image(systemName: tabBarImages[num])
-                                    .font(.system(size: 44, weight: .bold))
-                                    .foregroundColor(.red)
-                            } else if num == 0  {
-                                VStack{
-                                    Image(systemName: tabBarImages[num])
-                                        .font(.system(size: 20, weight: .bold))
-                                        .foregroundColor(selectedIndex == num ? Color(.black) : .init(white: 0.8))
-                                    Text(tabBarNames[num])
-                                            .font(.system(size: 11, weight: .bold))
-                                            .foregroundColor(selectedIndex == num ? Color(.black) : .init(white: 0.8))
-                                }
-                                
-                            } else {
-                                VStack{
-                                    Image(systemName: tabBarImages[num])
-                                        .font(.system(size: 24, weight: .bold))
-                                        .foregroundColor(selectedIndex == num ? Color(.black) : .init(white: 0.8))
-                                    Text(tabBarNames[num])
-                                            .font(.system(size: 11, weight: .bold))
-                                            .foregroundColor(selectedIndex == num ? Color(.black) : .init(white: 0.8))
-                                }
-                            }
-                            
-                            Spacer()
-                        })
+
+            Budget()
+                .tabItem{
+                    Image(systemName: "banknote.fill")
+                    Text("Budget")
                 }
-            }
+
+            Evaluation()
+                .tabItem{
+                    Image(systemName: "chart.bar.fill")
+                    Text("Evaluation")
+                }
+
+            Education()
+                .tabItem{
+                    Image(systemName: "book.closed.fill")
+                    Text("Education")
+                }
+
         }
     }
 }
 
-//TabView {
-//    Dashboard()
-//        .tabItem{
-//            Image(systemName: "house.fill")
-//            Text("Dashboard")
-//        }
+
+
+//VStack(spacing: 0){
+//    ZStack{
+//        Spacer()
+//            .fullScreenCover(isPresented: $shouldShowModel, content: {
+//                Button(action: {shouldShowModel.toggle()}, label: {
+//                    Text("Ini Input Expense Page")
+//                })
+//            })
 //
-//    Budget()
-//        .tabItem{
-//            Image(systemName: "banknote.fill")
-//            Text("Budget")
+//        switch selectedIndex{
+//        case 1 :
+//            Budget()
+//        case 3:
+//            Evaluation()
+//        case 4:
+//            Education()
+//        default:
+//            Dashboard()
 //        }
+//    }
+//    Divider()
+//        .padding(.bottom, 8)
+//    HStack{
+//        ForEach(0..<5) { num in
+//                Button(action:{
+//                    if num == 2 {
+//                        shouldShowModel.toggle()
+//                        return
+//                    }
+//                    selectedIndex = num
+//                }, label: {
+//                    Spacer()
 //
-//    Evaluation()
-//        .tabItem{
-//            Image(systemName: "chart.bar.fill")
-//            Text("Evaluation")
+//                    if num == 2 {
+//                        Image(systemName: tabBarImages[num])
+//                            .font(.system(size: 40, weight: .bold))
+//                            .foregroundColor(.blue)
+//                    } else if num == 0  {
+//                        VStack{
+//                            Image(systemName: tabBarImages[num])
+//                                .font(.system(size: 20, weight: .bold))
+//                                .foregroundColor(selectedIndex == num ? Color(.black) : .init(white: 0.8))
+//                            Text(tabBarNames[num])
+//                                    .font(.system(size: 11, weight: .bold))
+//                                    .foregroundColor(selectedIndex == num ? Color(.black) : .init(white: 0.8))
+//                        }
+//
+//                    } else {
+//                        VStack{
+//                            Image(systemName: tabBarImages[num])
+//                                .font(.system(size: 24, weight: .bold))
+//                                .foregroundColor(selectedIndex == num ? Color(.black) : .init(white: 0.8))
+//                            Text(tabBarNames[num])
+//                                    .font(.system(size: 11, weight: .bold))
+//                                    .foregroundColor(selectedIndex == num ? Color(.black) : .init(white: 0.8))
+//                        }
+//                    }
+//
+//                    Spacer()
+//                })
 //        }
-//
-//    Education()
-//        .tabItem{
-//            Image(systemName: "book.closed.fill")
-//            Text("Education")
-//        }
-//
+//    }
 //}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
