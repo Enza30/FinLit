907d41b5407660ea9b2eb39716e7f847970dbd24
Rezhaldo <85718279+Rezhaldo@users.noreply.github.com>
2021-11-15 13:11:35 +0700

    edit budget tab

--- FinLit/Views/Budget/topCardView.swift
+++ FinLit/Views/Budget/topCardView.swift
@@ -15,66 +15,67 @@ struct topCardView: View {
             
 //            VStack(alignment: .leading) {
                 HStack(alignment:.center){
-                    
+                Spacer()
                     bar()
                         .frame(width: 128, height: 128, alignment: .center)
 
                     Spacer(minLength: 50)
                     
-                    VStack(alignment: .leading) {
+                    VStack(alignment: .leading, spacing: 0) {
+                        Spacer(minLength: 5)
                         Text("Remaining Budget")
                             .font(.system(size: 14)).opacity(0.5)
-                            .frame(width: 180, height: 18, alignment: .topTrailing)
+                            .frame(width: 175, height: 18, alignment: .topTrailing)
                             .foregroundColor(Color.black)
-        
-                        
+                            
                         Text("RP.300.000")
                             .font(.system(size: 18)).bold()
-                            .frame(width: 180, height: 18, alignment: .topTrailing)
+                            .frame(width: 175, height: 18, alignment: .topTrailing)
                             .foregroundColor(Color.black)
+                        Spacer()
                         
                         Text("Base budget")
                             .font(.system(size: 14)).opacity(0.5)
-                            .frame(width: 180, height: 18, alignment: .topTrailing)
+                            .frame(width: 175, height: 18, alignment: .topTrailing)
                             .foregroundColor(Color.black)
                 
                         Text("RP.1.500.000 of 1.750.000")
                             .font(.system(size: 14))
                             .foregroundColor(Color.black)
-                            .frame(width: 180, height: 20, alignment: .topTrailing)
-                        
-                            
+                            .frame(width: 175, height: 20, alignment: .topTrailing)
+                        Spacer()
                             
                         HStack {
-                            Button(action: {}, label: {
-                                HStack {
-                                    Text("Add income")
-                                        .font(.system(size: 14))
-                                        .frame(width: 88, height: 29)
-                                        .background(Color("ActionColor"))
-                                        .cornerRadius(8)
-                                        
-                                    
-                                }
-                                .foregroundColor(Color.white)
-                            })
+//                            Button(action: {}, label: {
+//                                HStack {
+//                                    Text("Add income")
+//                                        .font(.system(size: 14))
+//                                        .frame(width: 88, height: 29)
+//                                        .background(Color("ActionColor"))
+//                                        .cornerRadius(8)
+//                                        
+//                                    
+//                                }
+//                                .foregroundColor(Color.white)
+//                            })
                             
-                            Button(action: {}, label: {
-                                HStack {
-                                    Text("Edit budget")
-                                        .font(.system(size: 14))
-                                        .frame(width: 88, height: 29)
-                                        .background(Color("ActionColor"))
-                                        .cornerRadius(8)
-                                        
-                                    
-                                }
-                                .foregroundColor(Color.white)
-                            })
+                            HStack {
+                                Spacer()
+                                Button(action: {}, label: {
+                                        Text("Edit budget")
+                                            .font(.system(size: 14))
+                                            .frame(width: 150, height: 29)
+                                            .background(Color("ActionColor"))
+                                            .cornerRadius(8)
+                                            .foregroundColor(.white)
+                                            
+                                })
+                            }
                             
                         }
                         
                     }
+                    .frame(height: 163)
 
                 }
                 .frame(height:164)
