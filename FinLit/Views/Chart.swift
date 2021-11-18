797d68999aa008ca60189335e7254168be6090a3
Farendza <farendza.muda@gmail.com>
2021-11-18 08:22:02 +0700

    New Chart addded

--- FinLit/Views/Chart.swift
+++ FinLit/Views/Chart.swift
@@ -1,55 +1,22 @@
-//
-//  Chart.swift
-//  FinLit
-//
-//  Created by Farendza Muda on 10/11/21.
-//
-import SwiftUICharts
+////
+////  Chart.swift
+////  FinLit
+////
+////  Created by Farendza Muda on 10/11/21.
+////
 import SwiftUI
+import SwiftUICharts
 
 struct Chart: View {
+    
+
+    var data2: [(String,Double)] = [("August 1",25000),("August 2",45000),("August 3",35000),("August 4",50000),("August 5",100000),("August 6",75000),("August 7",20000),("August 8",110000),("August 9",89000),("August 10",14000),("August 11",45000)]
+
+    
+
     var body: some View {
-        NavigationView{
-            ScrollView{
-                VStack(alignment: .leading){
-                    // Legend
-                    let expense = Legend(color: Color("ActionColor"), label: "Expense")
-//                    let iphone8 = Legend(color: .blue, label: "iPhone 8")
-//                    let iphone7 = Legend(color: .blue, label: "iPhone 7")
-//                    let iphone12 = Legend(color: .blue, label: "iPhone 12")
-//                    let iphone13 = Legend(color: .blue, label: "iPhone 13")
-//                    let iphone11 = Legend(color: .blue, label: "iPhone 11")
-                    
-                    // DataPoints
-                    let points: [DataPoint] = [
-                        .init(value: 20000, label: "1", legend: expense),
-                        .init(value: 75000, label: "2", legend: expense),
-                        .init(value: 75000, label: "3", legend: expense),
-                        .init(value: 50000, label: "4", legend: expense),
-                        .init(value: 10000, label: "5", legend: expense),
-                        .init(value: 45000, label: "6", legend: expense),
-                        .init(value: 80000, label: "7", legend: expense)
-                    ]
-                    //Bar
-                    Section(header: Text("Monthly Expense")) {
-                        BarChartView(dataPoints: points)
-                            .padding()
-                            .frame(height: UIScreen.main.bounds.size.height/3)
-                    }.padding(.leading)
-                    Divider()
-                    
-                    Text("Highest Expenses")
-                        .font(.headline)
-                        .padding(.leading)
-                    
-                    Spacer()
-                    
-                    kategori_baru()
-                }
-                    
-            }
-            .navigationTitle("Evaluation")
-        }
+        VStack {
+            BarChartView(data: ChartData(values: data2), title: "Expense August", legend: "", style: Styles.barChartStyleOrangeLight, form: CGSize(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height/2.5))
     }
 }
 
@@ -57,4 +24,5 @@ struct Chart_Previews: PreviewProvider {
     static var previews: some View {
         Chart()
     }
+}
 }
